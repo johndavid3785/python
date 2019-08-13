95183d50d6a9e4c6eb52c2000e4592207808ca87
johndavid3785 <37238397+johndavid3785@users.noreply.github.com>
2019-08-13 21:52:28 +0530

    Update findingrepeatednum.py

--- findingrepeatednum.py
+++ findingrepeatednum.py
@@ -1,9 +1,17 @@
 n=int(input())
-o=list(map(int,input().split()))
-for i in range(0,len(o)):
-        if o.count(i)>=2:
-            print(i,end=" ")
-        else:
-            break
-print("unique")
-            
+l =list(map(int,input().split()))
+l.sort()
+s=[]
+for i in range (len (l) -1):
+    if l[i] == l[i+1]:
+        s.append(l[i])
+    #elif print("unique"):
+        if l[i] in s:
+            if s.count(l[i])==1 :
+                print(l[i],end=" ")
+if s.count(l[i])==0:
+    print("unique")
+
+
+
+
